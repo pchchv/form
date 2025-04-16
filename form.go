dc0e875452009a9eb5408d7a3f7b6a820100a1be
pchchv <ipchchv@gmail.com>
2025-04-16 12:19:10 +0300

    Add a Mode type

--- form.go
+++ form.go
@@ -1,1 +1,9 @@
 package form
+
+const (
+	ModeImplicit Mode = iota // ModeImplicit tries to parse values for all fields that do not have an ignore '-' tag.
+	ModeExplicit             // ModeExplicit parses values for field with a field tag and that tag is not the ignore '-' tag.
+)
+
+// Mode specifies which mode the form decoder is to run
+type Mode uint8
