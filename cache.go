fba18254a6c4fbb070dad8928562cafc90349acc
pchchv <ipchchv@gmail.com>
2025-04-16 12:29:11 +0300

    Implement Len, Less and Swap methods for cacheFields

--- cache.go
+++ cache.go
@@ -8,3 +8,15 @@ type cachedField struct {
 }
 
 type cacheFields []cachedField
+
+func (s cacheFields) Len() int {
+	return len(s)
+}
+
+func (s cacheFields) Less(i, j int) bool {
+	return !s[i].isAnonymous
+}
+
+func (s cacheFields) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
