ce442671cdf408ecca28c357d20fd881e04ffb87
pchchv <ipchchv@gmail.com>
2025-04-16 12:37:30 +0300

    Implement parseStruct method for structCacheMap

--- cache.go
+++ cache.go
@@ -2,6 +2,8 @@ package form
 
 import (
 	"reflect"
+	"sort"
+	"strings"
 	"sync"
 	"sync/atomic"
 )
@@ -60,4 +62,58 @@ func (s *structCacheMap) Set(key reflect.Type, value *cachedStruct) {
 
 	nm[key] = value
 	s.m.Store(nm)
+}
+
+func (s *structCacheMap) parseStruct(mode Mode, current reflect.Value, key reflect.Type, tagName string) *cachedStruct {
+	s.lock.Lock()
+	// could have been multiple trying to access,
+	// but once first is done this ensures struct isn't parsed again
+	cs, ok := s.Get(key)
+	if ok {
+		s.lock.Unlock()
+		return cs
+	}
+
+	var idx int
+	var name string
+	var isOmitEmpty bool
+	var fld reflect.StructField
+	typ := current.Type()
+	numFields := current.NumField()
+	cs = &cachedStruct{fields: make([]cachedField, 0, 4)} // init 4, betting most structs decoding into have at aleast 4 fields
+	for i := 0; i < numFields; i++ {
+		isOmitEmpty = false
+		fld = typ.Field(i)
+		if fld.PkgPath != blank && !fld.Anonymous {
+			continue
+		}
+
+		if s.tagFn != nil {
+			name = s.tagFn(fld)
+		} else {
+			name = fld.Tag.Get(tagName)
+		}
+
+		if name == ignore || (mode == ModeExplicit && len(name) == 0) {
+			continue
+		}
+
+		// check for omitempty
+		if idx = strings.LastIndexByte(name, ','); idx != -1 {
+			isOmitEmpty = name[idx+1:] == "omitempty"
+			name = name[:idx]
+		}
+
+		if len(name) == 0 {
+			name = fld.Name
+		}
+
+		cs.fields = append(cs.fields, cachedField{idx: i, name: name, isAnonymous: fld.Anonymous, isOmitEmpty: isOmitEmpty})
+	}
+
+	sort.Sort(cs.fields)
+	s.Set(typ, cs)
+	s.lock.Unlock()
+
+	return cs
 }
