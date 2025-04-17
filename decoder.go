fc40c6eb9909d952054f428400dbf2aa8b36e0c5
pchchv <ipchchv@gmail.com>
2025-04-17 11:08:21 +0300

    Implement setError and setFieldByType methods

--- decoder.go
+++ decoder.go
@@ -6,9 +6,11 @@ import (
 	"net/url"
 	"reflect"
 	"strconv"
+	"time"
 )
 
 const (
+	errArraySize           = "array size of '%d' is larger than the maximum currently set on the decoder of '%d'. To increase this limit please see, SetMaxArraySize(size uint)"
 	errMissingEndBracket   = "invalid formatting for key '%s' missing ']' bracket"
 	errMissingStartBracket = "invalid formatting for key '%s' missing '[' bracket"
 )
@@ -216,4 +218,417 @@ func (d *decoder) findAlias(ns string) *recursiveData {
 		}
 	}
 	return nil
+}
+
+func (d *decoder) setError(namespace []byte, err error) {
+	if d.errs == nil {
+		d.errs = make(DecodeErrors)
+	}
+	d.errs[string(namespace)] = err
+}
+
+func (d *decoder) setFieldByType(current reflect.Value, namespace []byte, idx int) (set bool) {
+	var err error
+	v, kind := ExtractType(current)
+	arr, ok := d.values[string(namespace)]
+	if d.d.customTypeFuncs != nil {
+		if ok {
+			if cf, ok := d.d.customTypeFuncs[v.Type()]; ok {
+				val, err := cf(arr[idx:])
+				if err != nil {
+					d.setError(namespace, err)
+					return
+				}
+
+				v.Set(reflect.ValueOf(val))
+				set = true
+				return
+			}
+		}
+	}
+
+	switch kind {
+	case reflect.Interface:
+		if !ok || idx == len(arr) {
+			return
+		}
+
+		v.Set(reflect.ValueOf(arr[idx]))
+		set = true
+	case reflect.Ptr:
+		newVal := reflect.New(v.Type().Elem())
+		if set = d.setFieldByType(newVal.Elem(), namespace, idx); set {
+			v.Set(newVal)
+		}
+	case reflect.String:
+		if !ok || idx == len(arr) {
+			return
+		}
+
+		v.SetString(arr[idx])
+		set = true
+	case reflect.Uint, reflect.Uint64:
+		if !ok || idx == len(arr) || len(arr[idx]) == 0 {
+			return
+		}
+
+		var u64 uint64
+		if u64, err = strconv.ParseUint(arr[idx], 10, 64); err != nil {
+			d.setError(namespace, fmt.Errorf("invalid unsigned integer value '%s' type '%v' namespace '%s'", arr[idx], v.Type(), string(namespace)))
+			return
+		}
+
+		v.SetUint(u64)
+		set = true
+	case reflect.Uint8:
+		if !ok || idx == len(arr) || len(arr[idx]) == 0 {
+			return
+		}
+
+		var u64 uint64
+		if u64, err = strconv.ParseUint(arr[idx], 10, 8); err != nil {
+			d.setError(namespace, fmt.Errorf("invalid unsigned integer value '%s' type '%v' namespace '%s'", arr[idx], v.Type(), string(namespace)))
+			return
+		}
+
+		v.SetUint(u64)
+		set = true
+	case reflect.Uint16:
+		if !ok || idx == len(arr) || len(arr[idx]) == 0 {
+			return
+		}
+
+		var u64 uint64
+		if u64, err = strconv.ParseUint(arr[idx], 10, 16); err != nil {
+			d.setError(namespace, fmt.Errorf("invalid unsigned integer value '%s' type '%v' namespace '%s'", arr[idx], v.Type(), string(namespace)))
+			return
+		}
+
+		v.SetUint(u64)
+		set = true
+	case reflect.Uint32:
+		if !ok || idx == len(arr) || len(arr[idx]) == 0 {
+			return
+		}
+
+		var u64 uint64
+		if u64, err = strconv.ParseUint(arr[idx], 10, 32); err != nil {
+			d.setError(namespace, fmt.Errorf("invalid unsigned integer value '%s' type '%v' namespace '%s'", arr[idx], v.Type(), string(namespace)))
+			return
+		}
+
+		v.SetUint(u64)
+		set = true
+	case reflect.Int, reflect.Int64:
+		if !ok || idx == len(arr) || len(arr[idx]) == 0 {
+			return
+		}
+
+		var i64 int64
+		if i64, err = strconv.ParseInt(arr[idx], 10, 64); err != nil {
+			d.setError(namespace, fmt.Errorf("invalid integer value '%s' type '%v' namespace '%s'", arr[idx], v.Type(), string(namespace)))
+			return
+		}
+
+		v.SetInt(i64)
+		set = true
+	case reflect.Int8:
+		if !ok || idx == len(arr) || len(arr[idx]) == 0 {
+			return
+		}
+
+		var i64 int64
+		if i64, err = strconv.ParseInt(arr[idx], 10, 8); err != nil {
+			d.setError(namespace, fmt.Errorf("invalid integer value '%s' type '%v' namespace '%s'", arr[idx], v.Type(), string(namespace)))
+			return
+		}
+
+		v.SetInt(i64)
+		set = true
+	case reflect.Int16:
+		if !ok || idx == len(arr) || len(arr[idx]) == 0 {
+			return
+		}
+
+		var i64 int64
+		if i64, err = strconv.ParseInt(arr[idx], 10, 16); err != nil {
+			d.setError(namespace, fmt.Errorf("invalid integer value '%s' type '%v' namespace '%s'", arr[idx], v.Type(), string(namespace)))
+			return
+		}
+
+		v.SetInt(i64)
+		set = true
+	case reflect.Int32:
+		if !ok || idx == len(arr) || len(arr[idx]) == 0 {
+			return
+		}
+
+		var i64 int64
+		if i64, err = strconv.ParseInt(arr[idx], 10, 32); err != nil {
+			d.setError(namespace, fmt.Errorf("invalid integer value '%s' type '%v' namespace '%s'", arr[idx], v.Type(), string(namespace)))
+			return
+		}
+
+		v.SetInt(i64)
+		set = true
+	case reflect.Float32:
+		if !ok || idx == len(arr) || len(arr[idx]) == 0 {
+			return
+		}
+
+		var f float64
+		if f, err = strconv.ParseFloat(arr[idx], 32); err != nil {
+			d.setError(namespace, fmt.Errorf("invalid float value '%s' type '%v' namespace '%s'", arr[idx], v.Type(), string(namespace)))
+			return
+		}
+
+		v.SetFloat(f)
+		set = true
+	case reflect.Float64:
+		if !ok || idx == len(arr) || len(arr[idx]) == 0 {
+			return
+		}
+
+		var f float64
+		if f, err = strconv.ParseFloat(arr[idx], 64); err != nil {
+			d.setError(namespace, fmt.Errorf("invalid float value '%s' type '%v' namespace '%s'", arr[idx], v.Type(), string(namespace)))
+			return
+		}
+
+		v.SetFloat(f)
+		set = true
+	case reflect.Bool:
+		if !ok || idx == len(arr) {
+			return
+		}
+
+		var b bool
+		if b, err = parseBool(arr[idx]); err != nil {
+			d.setError(namespace, fmt.Errorf("Invalid Boolean Value '%s' Type '%v' Namespace '%s'", arr[idx], v.Type(), string(namespace)))
+			return
+		}
+
+		v.SetBool(b)
+		set = true
+	case reflect.Slice:
+		d.parseMapData()
+		// slice elements could be mixed eg. number and non-numbers Value[0]=[]string{"10"} and Value=[]string{"10","20"}
+		if ok && len(arr) > 0 {
+			var ol int
+			var varr reflect.Value
+			l := len(arr)
+			if v.IsNil() {
+				varr = reflect.MakeSlice(v.Type(), len(arr), len(arr))
+			} else {
+				ol = v.Len()
+				l += ol
+				if v.Cap() <= l {
+					varr = reflect.MakeSlice(v.Type(), l, l)
+				} else {
+					// preserve predefined capacity, possibly for reuse after decoding
+					varr = reflect.MakeSlice(v.Type(), l, v.Cap())
+				}
+
+				reflect.Copy(varr, v)
+			}
+
+			for i := ol; i < l; i++ {
+				newVal := reflect.New(v.Type().Elem()).Elem()
+				if d.setFieldByType(newVal, namespace, i-ol) {
+					set = true
+					varr.Index(i).Set(newVal)
+				}
+			}
+
+			v.Set(varr)
+		}
+
+		// maybe it's an numbered array i.e. Phone[0].Number
+		if rd := d.findAlias(string(namespace)); rd != nil {
+			var kv key
+			var varr reflect.Value
+			sl := rd.sliceLen + 1
+			// checking below for maxArraySize, but if array exists and already
+			// has sufficient capacity allocated then we do not check as the code
+			// obviously allows a capacity greater than the maxArraySize.
+			if v.IsNil() {
+				if sl > d.d.maxArraySize {
+					d.setError(namespace, fmt.Errorf(errArraySize, sl, d.d.maxArraySize))
+					return
+				}
+
+				varr = reflect.MakeSlice(v.Type(), sl, sl)
+			} else if v.Len() < sl {
+				if v.Cap() <= sl {
+					if sl > d.d.maxArraySize {
+						d.setError(namespace, fmt.Errorf(errArraySize, sl, d.d.maxArraySize))
+						return
+					}
+
+					varr = reflect.MakeSlice(v.Type(), sl, sl)
+				} else {
+					varr = reflect.MakeSlice(v.Type(), sl, v.Cap())
+				}
+
+				reflect.Copy(varr, v)
+			} else {
+				varr = v
+			}
+
+			for i := 0; i < len(rd.keys); i++ {
+				kv = rd.keys[i]
+				newVal := reflect.New(varr.Type().Elem()).Elem()
+				if kv.ivalue == -1 {
+					d.setError(namespace, fmt.Errorf("invalid slice index '%s'", kv.value))
+					continue
+				}
+
+				if d.setFieldByType(newVal, append(namespace, kv.searchValue...), 0) {
+					set = true
+					varr.Index(kv.ivalue).Set(newVal)
+				}
+			}
+
+			if !set {
+				return
+			}
+
+			v.Set(varr)
+		}
+	case reflect.Array:
+		d.parseMapData()
+		// array elements could be mixed eg. number and non-numbers Value[0]=[]string{"10"} and Value=[]string{"10","20"}
+		if ok && len(arr) > 0 {
+			var varr reflect.Value
+			l := len(arr)
+			overCapacity := v.Len() < l
+			if overCapacity {
+				// more values than array capacity,
+				// ignore values over capacity as it's possible some would just want
+				// to grab the first x number of elements;
+				// in the future strict mode logic should return an error
+				fmt.Println("warning number of post form array values is larger than array capacity, ignoring overflow values")
+			}
+			varr = reflect.Indirect(reflect.New(reflect.ArrayOf(v.Len(), v.Type().Elem())))
+			reflect.Copy(varr, v)
+			if v.Len() < len(arr) {
+				l = v.Len()
+			}
+
+			for i := 0; i < l; i++ {
+				newVal := reflect.New(v.Type().Elem()).Elem()
+				if d.setFieldByType(newVal, namespace, i) {
+					set = true
+					varr.Index(i).Set(newVal)
+				}
+			}
+
+			v.Set(varr)
+		}
+
+		// maybe it's an numbered array i.e. Phone[0].Number
+		if rd := d.findAlias(string(namespace)); rd != nil {
+			var kv key
+			var varr reflect.Value
+			overCapacity := rd.sliceLen >= v.Len()
+			if overCapacity {
+				// more values than array capacity, ignore values over capacity as it's possible some would just want
+				// to grab the first x number of elements; in the future strict mode logic should return an error
+				fmt.Println("warning number of post form array values is larger than array capacity, ignoring overflow values")
+			}
+
+			varr = reflect.Indirect(reflect.New(reflect.ArrayOf(v.Len(), v.Type().Elem())))
+			reflect.Copy(varr, v)
+			for i := 0; i < len(rd.keys); i++ {
+				kv = rd.keys[i]
+				if kv.ivalue >= v.Len() {
+					continue
+				}
+
+				newVal := reflect.New(varr.Type().Elem()).Elem()
+				if kv.ivalue == -1 {
+					d.setError(namespace, fmt.Errorf("invalid array index '%s'", kv.value))
+					continue
+				}
+
+				if d.setFieldByType(newVal, append(namespace, kv.searchValue...), 0) {
+					set = true
+					varr.Index(kv.ivalue).Set(newVal)
+				}
+			}
+
+			if !set {
+				return
+			}
+
+			v.Set(varr)
+		}
+	case reflect.Map:
+		var rd *recursiveData
+		d.parseMapData()
+		// no natural map support so skip directly to dm lookup
+		if rd = d.findAlias(string(namespace)); rd == nil {
+			return
+		}
+
+		var kv key
+		var existing bool
+		var mp reflect.Value
+		var mk reflect.Value
+		typ := v.Type()
+		if v.IsNil() {
+			mp = reflect.MakeMap(typ)
+		} else {
+			existing = true
+			mp = v
+		}
+
+		for i := 0; i < len(rd.keys); i++ {
+			newVal := reflect.New(typ.Elem()).Elem()
+			mk = reflect.New(typ.Key()).Elem()
+			kv = rd.keys[i]
+			if err := d.getMapKey(kv.value, mk, namespace); err != nil {
+				d.setError(namespace, err)
+				continue
+			}
+
+			if d.setFieldByType(newVal, append(namespace, kv.searchValue...), 0) {
+				set = true
+				mp.SetMapIndex(mk, newVal)
+			}
+		}
+
+		if !set || existing {
+			return
+		}
+
+		v.Set(mp)
+	case reflect.Struct:
+		typ := v.Type()
+		// if we get here then no custom time function declared so use RFC3339 by default
+		if typ == timeType {
+			if !ok || len(arr[idx]) == 0 {
+				return
+			}
+
+			t, err := time.Parse(time.RFC3339, arr[idx])
+			if err != nil {
+				d.setError(namespace, err)
+			}
+
+			v.Set(reflect.ValueOf(t))
+			set = true
+			return
+		}
+
+		d.parseMapData()
+		// must be recursing infinitly...but that's ok we caught it on the very first overun
+		if len(namespace) > d.maxKeyLen {
+			return
+		}
+
+		set = d.traverseStruct(v, typ, namespace)
+	}
+
+	return
 }
--- form.go
+++ form.go
@@ -1,5 +1,10 @@
 package form
 
+import (
+	"reflect"
+	"time"
+)
+
 const (
 	blank             = ""
 	ignore            = "-"
@@ -8,6 +13,8 @@ const (
 	ModeImplicit Mode = iota // ModeImplicit tries to parse values for all fields that do not have an ignore '-' tag.
 	ModeExplicit             // ModeExplicit parses values for field with a field tag and that tag is not the ignore '-' tag.
 )
+
+var timeType = reflect.TypeOf(time.Time{})
 
 // Mode specifies which mode the form decoder is to run
 type Mode uint8
