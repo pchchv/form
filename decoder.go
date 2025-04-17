54a1453a02617ed7d5e7e21c2b628b848328e0fb
pchchv <ipchchv@gmail.com>
2025-04-17 11:01:46 +0300

    Implement parseMapData and findAlias methods

--- decoder.go
+++ decoder.go
@@ -2,9 +2,15 @@ package form
 
 import (
 	"fmt"
+	"log"
 	"net/url"
 	"reflect"
 	"strconv"
+)
+
+const (
+	errMissingEndBracket   = "invalid formatting for key '%s' missing ']' bracket"
+	errMissingStartBracket = "invalid formatting for key '%s' missing '[' bracket"
 )
 
 type decoder struct {
@@ -114,4 +120,100 @@ func (d *decoder) getMapKey(key string, current reflect.Value, namespace []byte)
 	}
 
 	return
+}
+
+func (d *decoder) parseMapData() {
+	if len(d.dm) > 0 {
+		// already parsed
+		return
+	}
+
+	var i, idx, l int
+	var rd *recursiveData
+	var isNum, insideBracket bool
+	d.maxKeyLen = 0
+	d.dm = d.dm[0:0]
+	for k := range d.values {
+		if len(k) > d.maxKeyLen {
+			d.maxKeyLen = len(k)
+		}
+
+		for i = 0; i < len(k); i++ {
+			switch k[i] {
+			case '[':
+				idx = i
+				insideBracket = true
+				isNum = true
+			case ']':
+				if !insideBracket {
+					log.Panicf(errMissingStartBracket, k)
+				}
+
+				if rd = d.findAlias(k[:idx]); rd == nil {
+					l = len(d.dm) + 1
+					if l > cap(d.dm) {
+						dm := make(dataMap, l)
+						copy(dm, d.dm)
+						rd = new(recursiveData)
+						dm[len(d.dm)] = rd
+						d.dm = dm
+					} else {
+						l = len(d.dm)
+						d.dm = d.dm[:l+1]
+						rd = d.dm[l]
+						rd.sliceLen = 0
+						rd.keys = rd.keys[0:0]
+					}
+
+					rd.alias = k[:idx]
+				}
+
+				// is map + key
+				ke := key{
+					ivalue:      -1,
+					value:       k[idx+1 : i],
+					searchValue: k[idx : i+1],
+				}
+
+				// is key is number, most likely array key, keep track of just in case an array/slice
+				if isNum {
+					// no need to check for error, it will always pass
+					// as we have done the checking to ensure
+					// the value is a number ahead of time
+					var err error
+					ke.ivalue, err = strconv.Atoi(ke.value)
+					if err != nil {
+						ke.ivalue = -1
+					}
+
+					if ke.ivalue > rd.sliceLen {
+						rd.sliceLen = ke.ivalue
+
+					}
+				}
+
+				rd.keys = append(rd.keys, ke)
+				insideBracket = false
+			default:
+				// checking if not a number, 0-9 is 48-57 in byte, see for yourself fmt.Println('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+				if insideBracket && (k[i] > 57 || k[i] < 48) {
+					isNum = false
+				}
+			}
+		}
+
+		// if still inside bracket, that means no ending bracket was ever specified
+		if insideBracket {
+			log.Panicf(errMissingEndBracket, k)
+		}
+	}
+}
+
+func (d *decoder) findAlias(ns string) *recursiveData {
+	for i := 0; i < len(d.dm); i++ {
+		if d.dm[i].alias == ns {
+			return d.dm[i]
+		}
+	}
+	return nil
 }
