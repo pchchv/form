19121615b0c1f8ef0c9074bf91faf7fa60fa3fa4
pchchv <ipchchv@gmail.com>
2025-04-18 12:04:08 +0300

    Implement setError and setVal methods

--- encoder.go
+++ encoder.go
@@ -8,3 +8,22 @@ type encoder struct {
 	values    url.Values
 	namespace []byte
 }
+
+func (e *encoder) setError(namespace []byte, err error) {
+	if e.errs == nil {
+		e.errs = make(EncodeErrors)
+	}
+
+	e.errs[string(namespace)] = err
+}
+
+func (e *encoder) setVal(namespace []byte, idx int, vals ...string) {
+	arr, ok := e.values[string(namespace)]
+	if ok {
+		arr = append(arr, vals...)
+	} else {
+		arr = vals
+	}
+
+	e.values[string(namespace)] = arr
+}
