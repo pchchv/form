a7a8c3f72dbeca8a56dea52710c08b832726e0ee
pchchv <ipchchv@gmail.com>
2025-04-18 12:05:38 +0300

    Implement a getMapKey method

--- encoder.go
+++ encoder.go
@@ -1,12 +1,51 @@
 package form
 
-import "net/url"
+import (
+	"fmt"
+	"net/url"
+	"reflect"
+	"strconv"
+)
 
 type encoder struct {
 	e         *Encoder
 	errs      EncodeErrors
 	values    url.Values
 	namespace []byte
+}
+
+func (e *encoder) getMapKey(key reflect.Value, namespace []byte) (string, bool) {
+	v, kind := ExtractType(key)
+	if e.e.customTypeFuncs != nil {
+		if cf, ok := e.e.customTypeFuncs[v.Type()]; ok {
+			if arr, err := cf(v.Interface()); err != nil {
+				e.setError(namespace, err)
+				return "", false
+			} else {
+				return arr[0], true
+			}
+		}
+	}
+
+	switch kind {
+	case reflect.Interface, reflect.Ptr:
+		return "", false
+	case reflect.String:
+		return v.String(), true
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		return strconv.FormatUint(v.Uint(), 10), true
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return strconv.FormatInt(v.Int(), 10), true
+	case reflect.Float32:
+		return strconv.FormatFloat(v.Float(), 'f', -1, 32), true
+	case reflect.Float64:
+		return strconv.FormatFloat(v.Float(), 'f', -1, 64), true
+	case reflect.Bool:
+		return strconv.FormatBool(v.Bool()), true
+	default:
+		e.setError(namespace, fmt.Errorf("Unsupported Map Key '%v' Namespace '%s'", v.String(), namespace))
+		return "", false
+	}
 }
 
 func (e *encoder) setError(namespace []byte, err error) {
